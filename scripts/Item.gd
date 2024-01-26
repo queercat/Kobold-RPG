8be7934d273d21e0468916e7bf9a469384be915b
May Tusek <128516535+may-20i@users.noreply.github.com>
2024-01-26 03:18:46 -0800

    Creates item prefab.

---
+++ scripts/Item.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@export var _name: String
+@export var _sprite: Texture2D
+
+var sprite_node: Sprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"Sprite2D".texture = _sprite
