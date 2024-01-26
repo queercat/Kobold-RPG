50e8abf2973ec71380985f643cd82a3cc400e18e
May Tusek <128516535+may-20i@users.noreply.github.com>
2024-01-26 01:26:51 -0800

    Begins adding controlled animations.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 
+var animation_controler: AnimatedSprite2D
+var previous_direction: Vector2
+
 @export var speed = 200
 @export var friction = 0.1
 @export var acceleration = 0.1
+
+func _ready():
+	animation_controler = $"AnimatedSprite2D"
+	previous_direction = Vector2(0, 0)
 
 func get_input():
 	var input = Vector2()
@@ -15,7 +22,23 @@ func get_input():
 		input.y += 1
 	if Input.is_action_pressed("ui_up"):
 		input.y -= 1
+	
 	return input
+
+func _process(delta):
+	var direction = get_input()
+	
+	# Order matters. Left and right take precedences over top / down.
+	if direction.x != 0 && previous_direction.x != direction.x:
+		# animation_controler.play("walk_right")
+		pass
+	elif direction.y != 0 && previous_direction.y != direction.y:
+		if direction.y > 0:
+			animation_controler.play("idle_front")
+		else:
+			animation_controler.play("idle_back")
+	
+	previous_direction = direction
 
 func _physics_process(delta):
 	var direction = get_input()
