8c9e09692f2a89fbaf28e83fe81caa38849cece8
May Tusek <tusek.may@gmail.com>
2024-01-27 11:44:57 -0800

    Removes prefab.

--- scripts/dialogue_interaction.gd
+++ scripts/dialogue_interaction.gd
@@ -4,8 +4,9 @@ extends Node
 func _ready():
 	pass # Replace with function body.
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_body_entered(body):
+	print(body)
